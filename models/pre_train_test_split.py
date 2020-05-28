2e5a6f2a5eff0fb63c2753317d03e7ae0f0d67cd
MAJDI FLAH <49450243+Majdi1994@users.noreply.github.com>
2020-05-28 19:59:10 -0400

    Update pre_train_test_split.py

--- models/pre_train_test_split.py
+++ models/pre_train_test_split.py
@@ -34,9 +34,45 @@ def trainer(model, train_dl, test_dl, data_id, config, params):
     y_true = torch.stack(y_true).view(-1)
     y_pred = torch.stack(y_pred).view(-1)
     print('=' * 50)
-    print(f'\t  Performance on test set:{data_id}::: Loss: {test_loss:.3f} ')
+    print(f'\t  Performance on test set:{data_id}::: Loss: {test_loss:.3f} ')#| Score: {test_score:7.3f}')
     print('=' * 50)
     print(classification_report(y_true, y_pred, target_names=target_names))
+    print(confusion_matrix(y_true,y_pred))
+    ##### Plotting the confusion matrix
+    def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
+        print(cm)
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, cm[i, j],
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+    ##### Compute confusion matrix
+    import itertools 
+    cnf_matrix = (confusion_matrix(y_true, y_pred))
+    np.set_printoptions(precision=2)
+    # Plot normalized confusion matrix
+    plt.figure()
+    # Plot non-normalized confusion matrix
+    plot_confusion_matrix(cnf_matrix, classes=target_names, title='Confusion matrix')
+    plt.show()    
+    print('The classification accuracy is =', accuracy_score(y_true, y_pred , normalize=True))
+    print('The f1 score is = ', f1_score(y_true, y_pred, average='weighted'))
+    print('The MCC Coefficient is =', matthews_corrcoef(y_true, y_pred))
+    # print('The ROC_AUC scrore is', roc_auc_score(y_true, y_scores))
     print('| End of Pre-training  |')
     print('=' * 50)
-    return model
+    return model    
