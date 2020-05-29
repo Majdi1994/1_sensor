55995e65eac51395c0ce90806ae29b09eb408c17
MAJDI FLAH <49450243+Majdi1994@users.noreply.github.com>
2020-05-28 20:03:59 -0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -9,73 +9,35 @@ class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
     
-#class CNN_1D(nn.Module):
-#    def __init__(self, input_dim, hidden_dim,dropout):
-#        super(CNN_1D, self).__init__()
-#        self.hidden_dim = hidden_dim
-#        self.input_dim = input_dim
-#        self.dropout=dropout
-#        self.encoder = nn.Sequential(
-#            
-#            nn.Conv1d(self.input_dim, 8, kernel_size=11, stride=1, padding=1, dilation=1),
-#            nn.BatchNorm1d(8),
-#            nn.ReLU(),
-#           
-#            nn.Conv1d(8, 8, kernel_size=3, stride=2, padding=1, dilation=1),
-#            
-#            Flatten(),
-#            nn.Linear(4064, self.hidden_dim))
-#        
-#        self.Classifier= nn.Sequential(  
-#                   
-#            nn.Dropout(p=self.dropout),
-#            
-#            nn.Linear(self.hidden_dim, self.hidden_dim//2),
-#            nn.ReLU(),
-#            nn.Dropout(p=self.dropout),
-#            
-#            nn.Linear(self.hidden_dim//2, 11))
-#        
-#    def forward(self, src):
-#        features = self.encoder(src)
-#        predictions = self.Classifier(features)
-#        return predictions, features
-        
 class CNN_1D(nn.Module):
-    def __init__(self, input_dim, hidden_dim, dropout):
+    def __init__(self, input_dim, hidden_dim,dropout):
         super(CNN_1D, self).__init__()
         self.hidden_dim = hidden_dim
         self.input_dim = input_dim
         self.dropout=dropout
         self.encoder = nn.Sequential(
+            
             nn.Conv1d(self.input_dim, 8, kernel_size=11, stride=1, padding=1, dilation=1),
             nn.BatchNorm1d(8),
             nn.ReLU(),
+           
             nn.Conv1d(8, 8, kernel_size=3, stride=2, padding=1, dilation=1),
-            nn.BatchNorm1d(8),
-            nn.ReLU(),
-            nn.Conv1d(8, 8, kernel_size=3, stride=2, padding=1, dilation=1),
-            nn.BatchNorm1d(8),
-            nn.ReLU(),
-            nn.Conv1d(8, 8, kernel_size=3, stride=2, padding=1, dilation=1),
-            nn.BatchNorm1d(8),
-            nn.ReLU(),
-            nn.Conv1d(8, 8, kernel_size=3, stride=2, padding=1, dilation=1),
-            nn.BatchNorm1d(8),
-            nn.ReLU(),
-            nn.Conv1d(8, 8, kernel_size=3, stride=2, padding=1, dilation=1),
+            
             Flatten(),
-            nn.Linear(256, self.hidden_dim//4))
-        
-        self.Classifier= nn.Sequential(
+            nn.Linear(4064, self.hidden_dim))
+       
+        self.Classifier= nn.Sequential(  
+                   
+            nn.Dropout(p=self.dropout),
+            
+            nn.Linear(self.hidden_dim, self.hidden_dim//2),
             nn.ReLU(),
             nn.Dropout(p=self.dropout),
-            nn.Linear(self.hidden_dim//4, self.hidden_dim//8),
-            nn.ReLU(),
-            nn.Dropout(p=self.dropout),
-            nn.Linear(self.hidden_dim//8, 11))
+            
+            nn.Linear(self.hidden_dim//2, 11))
         
     def forward(self, src):
         features = self.encoder(src)
         predictions = self.Classifier(features)
-        return predictions, features   
+        return predictions, features
+       
